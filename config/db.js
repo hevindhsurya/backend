3effbf1ec66ee67917d1f9344ad941e9d49b2227
hevindhsurya <hevindhsurya@gmail.com>
2025-06-04 18:52:33 +0530

    add

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://hevindhsurya:hevindhsurya@auth-system.wgzahet.mongodb.net/Project?retryWrites=true&w=majority&appName=Auth-System");
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
