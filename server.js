5abdcc8e7f60c79a205490711e48d774f194de68
hevindhsurya <hevindhsurya@gmail.com>
2025-06-04 18:21:30 +0530

    add

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const connectDB = require('./config/db');
 require('dotenv').config();
 
 connectDB();
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+app.listen(8080, () => {
+  console.log(`Server running on port ${8080}`);
 });
